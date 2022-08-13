eba0cd4fd77620d3e712080951aa76d8eb534d9f
Sangkun-svg <gims4752@gmail.com>
2022-08-13 21:51:56 +0900

    feat : generate default user table

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { faker } from "@faker-js/faker";
 
-function App() {
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  TableHead,
+  Paper,
+} from "@mui/material";
+import styled from "styled-components";
+
+export const App = () => {
+  const tableCells = ["Name", "E-mail", "Address(City)", "Phone"];
+  const {
+    internet: { userName, email },
+    address: { cityName },
+    phone: { phoneNumber },
+  } = faker;
+  faker.setLocale("ko");
+  const user = Array(50)
+    .fill(null)
+    .map(() => ({
+      name: userName(),
+      email: email(),
+      address: cityName(),
+      phone: phoneNumber("010-####-####"),
+    }));
+  user.map((el) => console.log(el));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <TableContainer component={Paper}>
+        <Table size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              {tableCells.map((col) => {
+                return <TableCell align="center">{col}</TableCell>;
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {user.map((user) => {
+              const { name, email, address, phone } = user;
+              return (
+                <TableRow>
+                  <TableCell align="center">{name}</TableCell>
+                  <TableCell align="center">{email}</TableCell>
+                  <TableCell align="center">{address}</TableCell>
+                  <TableCell align="center">{phone}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Wrapper>
   );
-}
-
-export default App;
+};
+const Wrapper = styled.div`
+  width: 70%;
+  margin-left: 15%;
+  margin-top: 5%;
+  height: auto;
+  border: 2px solid gray;
+`;
