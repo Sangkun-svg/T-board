71194f63fe1e132f3937b47a7dedad9bb67d9bbf
Sangkun-svg <gims4752@gmail.com>
2022-08-18 03:29:50 +0900

    design : correct a typo

--- server/src/model/User.ts
+++ server/src/model/User.ts
@@ -1,10 +1,10 @@
 import { Sequelize, Model, DataTypes } from "sequelize";
 
 const sequelize = new Sequelize("sqlite::memory:");
-const { STRING } = DataTypes;
-export const User = sequelize.define("User", {
-  username: STRING,
+const { STRING, INTEGER } = DataTypes;
+export const User = sequelize.define("users", {
+  name: STRING,
   email: STRING,
   address: STRING,
-  Phone: STRING,
+  phone: STRING,
 });
