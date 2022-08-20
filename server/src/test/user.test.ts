f4a991c2c16e35fff31bd42107c63f79a50e08f5
Sangkun-svg <gims4752@gmail.com>
2022-08-20 19:37:00 +0900

    test : generate user create test

--- server/src/test/user.test.ts
+++ server/src/test/user.test.ts
@@ -1,9 +1,46 @@
-test("Dummy Success test", () => {
-  const add = 1 + 2;
-  expect(add).toBe(3);
+import "regenerator-runtime";
+import { faker } from "@faker-js/faker";
+import { userServiceInstance } from "../service";
+import { dbConfig } from "../db/sequelize";
+
+faker.setLocale("ko");
+const {
+  internet: { userName, email },
+  address: { cityName },
+  phone: { number },
+} = faker;
+
+beforeAll(async () => {
+  await dbConfig
+    .authenticate()
+    .then(() => {
+      console.info("connected to db");
+      return dbConfig.drop();
+    })
+    .catch((err) => {
+      console.error("Errordb ", err);
+      throw "error";
+    });
 });
 
-test("Dummy fail test", () => {
-  const add = 1 + 2;
-  expect(add).toBe(1);
+test("User create test", async () => {
+  const mockData = {
+    name: userName(),
+    email: email(),
+    address: cityName(),
+    phone: number("010-####-####"),
+  };
+  const users = await userServiceInstance.create(mockData);
+  expect(!!users).toBe(true);
+});
+
+test("findAll users test", async () => {
+  const users: any = await userServiceInstance.getUsers();
+  console.log(Boolean(users));
+  console.log(users);
+  expect(!!users).toBe(true);
+});
+
+afterAll(() => {
+  dbConfig.close();
 });
