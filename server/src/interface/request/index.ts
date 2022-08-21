5fc10b5d3bc1b56a1af6765e87913db7f431404e
Sangkun-svg <gims4752@gmail.com>
2022-08-21 20:57:12 +0900

    feat : generate interface about parameter

---
+++ server/src/interface/request/index.ts
@@ -0,0 +1,1 @@
+export { IUserCreate, IUserUpdate } from "./userRequest";
---
+++ server/src/interface/request/userRequest.ts
@@ -0,0 +1,13 @@
+export interface IUserCreate {
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+}
+export interface IUserUpdate {
+  id: number;
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+}
