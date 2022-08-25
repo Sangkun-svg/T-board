fc76e6e73623f50140a6543ffc9584551a2c5994
Sangkun-svg <gims4752@gmail.com>
2022-08-25 19:43:14 +0900

    feat : change route apis

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -1,1 +1,1 @@
-export {} from "";
+export { userRouter } from "./userRouter";
--- server/src/router/userRouter.ts
+++ server/src/router/userRouter.ts
@@ -62,6 +62,7 @@ const getUserById = (req: Request, res: Response, next: NextFunction) => {
 };
 
 // TODO: refactor for RESTful & graceful request method
-userRouter.route("/users").get(getUsers);
-userRouter.route("/users/create").get(create);
-userRouter.route("/users/:id").get(getUserById).delete(deleteUser).put(update);
+userRouter.route("/").get(getUsers);
+userRouter.route("/create").get(create);
+userRouter.route("/update").put(update);
+userRouter.route("/:id").get(getUserById).delete(deleteUser);
