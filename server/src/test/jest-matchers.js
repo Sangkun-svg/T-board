b7d8adcf984a358b2e2b06f5bf2ea8726cb0e50e
Sangkun-svg <gims4752@gmail.com>
2022-09-06 00:59:53 +0900

    design : add reference about test matcher methods

--- server/src/test/jest-matchers.js
+++ server/src/test/jest-matchers.js
@@ -1,3 +1,14 @@
+let thisis = "Matchers Ref";
+// Reference: https://jestjs.io/docs/expect
+//            https://inpa.tistory.com/entry/JEST-%F0%9F%93%9A-jest-%EA%B8%B0%EB%B3%B8-%EB%AC%B8%EB%B2%95-%EC%A0%95%EB%A6%AC
+
+thisis = "the other Refs";
+/* Reference: https://crmrelease.tistory.com/106 
+              https://crmrelease.tistory.com/category/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-Web/Jest%28TDD%29 
+              https://www.daleseo.com/jest-basic/
+              https://brunch.co.kr/@pubjinson/16
+*/
+
 expect().toBe;
 expect().toEqual;
 expect().toStrictEqual;
