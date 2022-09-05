ed1cd2d1b6388ccdf3826c14aba828737246808b
Sangkun-svg <gims4752@gmail.com>
2022-09-06 00:45:11 +0900

    build : install cross-env for window user & modify test commands

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -8,9 +8,10 @@ import type { Config } from "@jest/types";
 
 // Sync object
 const config: Config.InitialOptions = {
-  verbose: true,
   preset: "ts-jest", // 이 부분에서 ts-jest를 사용한다고 알려준다
   testEnvironment: "node", //테스트 환경 'node' 환경을 사용한다 알려줌
   testMatch: ["**/test/*.test.(ts|tsx)"],
+  verbose: true,
+  forceExit: true,
 };
 export default config;
