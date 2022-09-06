0ce3c2cca725db0036a40c03b5148faefd61578d
Sangkun-svg <gims4752@gmail.com>
2022-09-06 19:38:24 +0900

    feat : use PORT declared in dotenv file

--- mock/src/server.ts
+++ mock/src/server.ts
@@ -1,6 +1,7 @@
 import { app } from "./app";
+import "dotenv/config";
 
-const PORT = 9000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server listening port on ${PORT}`);
   console.log(`default location : http://localhost:${PORT}/`);
