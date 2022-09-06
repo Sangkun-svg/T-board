c59a6d55c49d124eb8657ed454efdbeb6a60fa46
Sangkun-svg <gims4752@gmail.com>
2022-09-07 00:52:21 +0900

    feat : generate constant for testing

---
+++ mock/src/constant/forTest.ts
@@ -0,0 +1,10 @@
+export const MOCK_USER_ID_1 = {
+  id: 1,
+  name: "인용_추54",
+  email: ".96@naver.com",
+  address: "부평",
+  phone: "010-6244-7976",
+  createdAt: "2022-08-21T08:59:33.000Z",
+  updatedAt: "2022-08-31T11:05:25.000Z",
+  isDeleted: true,
+};
