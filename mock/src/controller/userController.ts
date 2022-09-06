3f9ebaa1ac57a888a4993d5e36bcfc24ff074fc9
Sangkun-svg <gims4752@gmail.com>
2022-09-06 19:41:12 +0900

    faet : get files from server folder for  testing in same situation

---
+++ mock/src/controller/userController.ts
@@ -0,0 +1,55 @@
+//TODO: study : tsoa란? & tsoa를 활용한 restfut api
+import {
+  Body,
+  Path,
+  Controller,
+  Get,
+  Put,
+  Post,
+  Delete,
+  Route,
+  SuccessResponse,
+  Patch,
+} from "tsoa";
+import { userService } from "../service";
+
+@Route("member")
+@SuccessResponse("200", "successfully")
+export class UserController extends Controller {
+  private static instance: UserController;
+  private userService = userService;
+
+  public static getInstance() {
+    if (!UserController.instance) {
+      UserController.instance = new UserController();
+    }
+    return UserController.instance;
+  }
+
+  @Get("/")
+  public async getUsers(): Promise<any> {
+    const result = await this.userService.getUsers();
+    return result;
+  }
+  @Get("/{id}")
+  public async getUserById(id: number): Promise<any> {
+    const result = await this.userService.getUserById(id);
+    return result;
+  }
+  @Post("/create")
+  public async create(data: Object | any): Promise<any> {
+    const result = await this.userService.create(data);
+    return result;
+  }
+  @Put("/update")
+  public async update(data: Object | any): Promise<any> {
+    const result = await this.userService.update(data);
+    return result;
+  }
+  @Patch("/delete")
+  public async delete(id: number): Promise<any> {
+    const result = await this.userService.softDelete(id);
+    return result;
+  }
+}
+export const userController = UserController.getInstance();
---
+++ mock/src/service/userService.ts
@@ -0,0 +1,109 @@
+// [Seqeulize Transaction Reference]
+// https://runebook.dev/ko/docs/sequelize/manual/transactions
+// https://www.ultimateakash.com/blog-details/IiwzPGAKYAo=/How-to-implement-Transactions-in-Sequelize-&-Node.Js-(Express)
+import { Sequelize } from "../db/sequelize";
+import { User } from "../model";
+import { IUserCreate, IUserUpdate } from "../interface/request";
+import { IUser } from "../interface/response";
+
+class UserService {
+  private static instance: UserService;
+  private contructor() {}
+  public static getInstance() {
+    if (!UserService.instance) {
+      UserService.instance = new UserService();
+    }
+    return UserService.instance;
+  }
+
+  public async create(obj: IUserCreate | any) {
+    const t = await Sequelize.transaction();
+    try {
+      const user = await User.create(obj);
+      t.commit();
+      return user;
+    } catch (error) {
+      await t.rollback();
+      throw new Error(); //TODO: need to error handling
+    }
+  }
+
+  public async update(obj: IUserUpdate) {
+    const t = await Sequelize.transaction();
+    try {
+      const result = await User.update(
+        {
+          name: obj.name,
+          phone: obj.phone,
+          email: obj.email,
+          address: obj.address,
+        },
+        {
+          where: {
+            id: obj.id,
+          },
+        }
+      );
+      t.commit();
+      return result;
+    } catch (error) {
+      await t.rollback();
+      throw new Error(); //TODO: need to error handling
+    }
+  }
+
+  public async softDelete(id: number) {
+    const t = await Sequelize.transaction();
+    try {
+      const result = await User.update(
+        { isDeleted: true },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+      t.commit();
+      return result;
+    } catch (error) {
+      await t.rollback();
+      throw new Error(); //TODO: need to error handling
+    }
+  }
+
+  public async hardDelete(id: number) {
+    const t = await Sequelize.transaction();
+    try {
+      const result = await User.destroy({
+        where: {
+          id: id,
+        },
+      });
+      t.commit();
+      return result;
+    } catch (error) {
+      await t.rollback();
+      throw new Error(); //TODO: need to error handling
+    }
+  }
+
+  public async getUsers(): Promise<IUser[]> {
+    try {
+      const users: IUser[] | any = await User.findAll();
+      return users;
+    } catch (error) {
+      console.error(error);
+      throw new Error();
+    }
+  }
+
+  public async getUserById(id: number): Promise<IUser> {
+    try {
+      const user: IUser | any = await User.findByPk(id);
+      return user;
+    } catch (error) {
+      throw new Error();
+    }
+  }
+}
+export const userService = UserService.getInstance();
