f539ccf14ebbb8a9fd5b7fc3d86048141b559819
Sangkun-svg <gims4752@gmail.com>
2022-09-06 19:39:06 +0900

    build : init & connect db using sequelize

---
+++ mock/src/db/sequelize.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import * as sequelize from "sequelize";
+
+export const Sequelize = new sequelize.Sequelize(
+  process.env.DB_DATABASE || "user",
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "1234",
+  {
+    // logging: true,
+    logging: false,
+    port: Number(process.env.DB_PORT) || 3306,
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+    pool: {
+      min: 1,
+      max: 5,
+      acquire: 100000,
+      idle: 10000,
+    },
+    define: {
+      // don't add the timestamp attributes (updatedAt, createdAt)
+      timestamps: true,
+      // If don't want createdAt
+      // createdAt: false,
+      // If don't want updatedAt
+      // updatedAt: false,
+    },
+  }
+);
