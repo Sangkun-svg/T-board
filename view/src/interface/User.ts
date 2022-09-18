6df86df2c63f3a90f80c2c7b5858ccb733343476
Sangkun-svg <gims4752@gmail.com>
2022-09-18 19:25:58 +0900

    feat : generate User inferface

---
+++ view/src/interface/User.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  id: string;
+  password: string;
+  isPassword: boolean;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  showPassword: boolean;
+}
