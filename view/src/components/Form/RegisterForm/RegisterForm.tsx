3ad17c37c300734cdd18026bfc31240aca5d1830
Sangkun-svg <gims4752@gmail.com>
2022-09-18 19:47:33 +0900

    rollback : 초심을 찾아서..

--- view/src/components/Form/RegisterForm/RegisterForm.tsx
+++ view/src/components/Form/RegisterForm/RegisterForm.tsx
@@ -6,118 +6,9 @@ import {
   Input,
   IconButton,
 } from "@mui/material";
-import { useState } from "react";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-import _ from "lodash";
+import styled from "styled-components";
 
-export const RegisterForm = () => {
-  const [data, setData] = useState({
-    id: "",
-    password: "",
-    name: "",
-    email: "",
-    phone: "",
-    address: "",
-    showPassword: false,
-  });
-
-  const handleChange = (prop) => (event) => {
-    setData({ ...data, [prop]: event.target.value });
-  };
-
-  const handlePhoneNumberFormat = (prop) => (event) => {
-    const regex = /^[0-9]{0,11}$/;
-    let inputValue = event.target.value;
-    if (regex.test(inputValue)) {
-      setData({
-        ...data,
-        [prop]: inputValue,
-      });
-    }
-  };
-
-  const handleClickShowPassword = () => {
-    setData({
-      ...data,
-      showPassword: !data.showPassword,
-    });
-  };
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
-
-  const handleClickCreateUser = () => {
-    console.log("create");
-  };
-
-  return (
-    <Box sx={style}>
-      <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
-        <TextField
-          id="id"
-          variant="standard"
-          type="text"
-          value={data.id}
-          placeholder="ID"
-          onChange={handleChange("id")}
-          // error={data.id === "" ? true : false} TODO: handling error state when click the create btn
-        />
-      </FormControl>
-
-      <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
-        <Input
-          id="password"
-          type={data.showPassword ? "text" : "password"}
-          value={data.password}
-          placeholder="Password"
-          onChange={handleChange("password")}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-              >
-                {data.showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-        />
-      </FormControl>
-      <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
-        <Input
-          id="email"
-          type="email"
-          value={data.email}
-          placeholder="Sangkun@gmail.com"
-          onChange={handleChange("email")}
-          // error={data.email === "" ? true : false} TODO: handling error state when click the create btn
-        />
-      </FormControl>
-      <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
-        <Input
-          id="address"
-          type="address"
-          value={data.address}
-          placeholder="Seoul , Republic of Korea"
-          onChange={handleChange("address")}
-          // error={data.address === "" ? true : false} TODO: handling error state when click the create btn
-        />
-      </FormControl>
-      <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
-        <TextField
-          inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-          id="phone"
-          type="text"
-          value={data.phone}
-          placeholder="010-0000-0000"
-          onChange={handlePhoneNumberFormat("phone")}
-        />
-      </FormControl>
-    </Box>
-  );
-};
+import { IUser } from "../../../interface";
 const style = {
   position: "absolute",
   top: "50%",
@@ -128,4 +19,20 @@ const style = {
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
+};
+export const RegisterForm = () => {
+  // return <BoxCustom>Createing..</BoxCustom>;
+  return (
+    <Box sx={style}>
+      <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
+        <TextField
+          id="id"
+          variant="standard"
+          type="text"
+          placeholder="ID"
+          // error={data.id === "" ? true : false} TODO: handling error state when click the create btn
+        />
+      </FormControl>
+    </Box>
+  );
 };
